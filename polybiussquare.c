033f63a1dc5d7459df55cc9887d2299384571e49
Henry Hall <henrydh@byu.edu>
2021-12-10 11:02:06 -0700

    Initial

---
+++ polybiussquare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n",argc);
+    printf("%s\n",argv[1]); // I like to print the command line arguments so I know what's going on during dev.
+
+    printf("TODO: find out how to get all 100 printable characters.\n");
+    printf("TODO: build \"array\" of characters.\n");
+    printf("TODO: figure out structure of program.\n");
+    printf("TODO: implement encryption.\n");
+    printf("TODO: finalize structure.\n");
+    printf("TODO: implement decryption.\n");
+    return 0;
+}
